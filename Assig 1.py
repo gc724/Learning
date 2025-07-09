95c1f46e18d71422b2937bcb488ca0b2a71fa2e1
gc724 <sahnj09@gmail.com>
2025-07-09 22:19:29 +0530

    Add files via upload

---
+++ Assig 1.py
@@ -0,0 +1,20 @@
+#Welcome User
+fname=input("Enter your first name: ")
+lname=input("Enter your last name: ")
+fname=str(fname)
+lname=str(lname)
+fullname=fname+" "+lname
+
+
+print("\nHello, "+ fullname +"! Welcome to Python\n")
+
+
+#Asking to enter numbers from user
+a=input("Enter the first number:  ")
+b=input("Enter the second number: ")
+a=float(a)
+b=float(b)
+print("\nAdiition: ", a+b)
+print("Subtraction: ", a-b)
+print("Multiplication: ", a*b)
+print("Divison: ", a/b)
